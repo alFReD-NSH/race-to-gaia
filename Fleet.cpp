3eb5858e5b0b4d92672f68affe8eb38e49e19b4a
Farid Neshat <FaridN_SOAD@yahoo.com>
2016-04-19 14:49:34 +0800

    Add the interface

--- Fleet.cpp
+++ Fleet.cpp
@@ -1,8 +1,110 @@
-#include <iostream>
+#include <vector>
+#include "Fleet.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
     return 0;
 }
+
+int Ship::getEnergyConsumption() const {
+    return 0;
+}
+
+int Ship::getWeight() const {
+    return 0;
+}
+
+int Ship::getCost() const {
+    return 0;
+}
+
+string Ship::getTypeName() const {
+    return std::basic_string<char, char_traits<char>, allocator<char>>();
+}
+
+bool Ship::isDestroyed() const {
+    return false;
+}
+
+
+int Fleet::getWeight() const {
+    return 0;
+}
+
+int Fleet::getEnergyConsumption() const {
+    return 0;
+}
+
+int Fleet::getColonistCount() const {
+    return 0;
+}
+
+int Fleet::getCost() const {
+    return 0;
+}
+
+int Fleet::EnergyProduction() const {
+    return 0;
+}
+
+int Fleet::countProtectedShips() const {
+    return 0;
+}
+
+bool Fleet::hasMedic() const {
+    return false;
+}
+
+string Fleet::getCorporationName() const {
+    return std::basic_string<char, char_traits<char>, allocator<char>>();
+}
+
+vector<Ship *> Fleet::protectedShips() const {
+    return std::vector<Ship *>();
+}
+
+vector<Ship *> Fleet::unprotectedShips() const {
+    return std::vector<Ship *>();
+}
+
+vector<Ship *> Fleet::colonyShips() const {
+    return std::vector<Ship *>();
+}
+
+vector<Ship *> Fleet::shipList() const {
+    return std::vector<Ship *>();
+}
+
+void Fleet::destroyShip(Ship *i) {
+
+}
+
+
+int ColonyShip::getColonistCount() const {
+    return 0;
+}
+
+void ColonyShip::infect() {
+
+}
+
+bool ColonyShip::isInfected() const {
+    return false;
+}
+
+
+int SolarSailShip::getEnergyProduction() const {
+    return 0;
+}
+
+
+int MilitaryEscortShip::getNrProtected() const {
+    return 0;
+}
+
+
+Fleet *userInterfaceCreateFleet() {
+    return nullptr;
+}
+
---
+++ Fleet.h
@@ -0,0 +1,51 @@
+#ifndef RACE_TO_GAIA_FLEET_H
+#define RACE_TO_GAIA_FLEET_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Ship {
+    int getEnergyConsumption() const; // Returns energy consumption of a ship
+    int getWeight() const; // Returns weight of a ship
+    int getCost() const; // Returns cost of a ship
+    string getTypeName() const; // Returns the ship type, e.g. Ferry, Cruiser, or Ebulient. Note that spelling mistakes may effect your grade!
+    bool isDestroyed() const; // Returns true if the ship is destroyed, false otherwise
+};
+
+class Fleet {
+    int getWeight() const; // Returns cumulative weight of fleet
+    int getEnergyConsumption() const; // Returns cumulative energy consumption of fleet
+    int getColonistCount() const; // Returns cumulative colonist count of fleet
+    int getCost() const; // Returns cumulative fleet cost
+    int EnergyProduction() const; // Returns cumulative energy production of fleet
+    int countProtectedShips() const; // Returns nr of colony ships protected in fleet
+    bool hasMedic() const; // Returns True if the fleet has a medic ship, false otherwise
+    string getCorporationName() const; // Returns your chosen name of your corporation.
+    vector<Ship *> protectedShips() const;
+
+    // Returns a vector with ship numbers of protected colony ships
+    vector<Ship *> unprotectedShips() const; // Returns a vector with ship numbers of unprotected colony ships
+    vector<Ship *> colonyShips() const; // Returns a vector with ship numbers of all ships that are a colony ship
+    vector<Ship *> shipList() const; // Returns a vector with all ships in the fleet
+    void destroyShip(Ship *i); // Removes ship i from the fleet
+};
+
+class ColonyShip : public Ship {
+    int getColonistCount() const; // Returns nr of colonists of a ship
+    void infect(); // Infects a colony ship
+    bool isInfected() const; // Returns True if the ship is infected with a disease, False otherwise
+};
+
+class SolarSailShip : public Ship {
+    int getEnergyProduction() const; // Returns energy production of Solar Sail Ship
+};
+
+class MilitaryEscortShip : public Ship {
+    int getNrProtected() const;   // Returns nr of colony ships protected by this ship
+};
+
+Fleet *userInterfaceCreateFleet();
+
+#endif //RACE_TO_GAIA_FLEET_H
